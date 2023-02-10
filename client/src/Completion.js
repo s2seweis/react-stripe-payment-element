22315ca1c97e578396d0a425f7693f8c59f3ae4b
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-02-10 09:32:58 +0100

    upload stripe-payment-element

---
+++ client/src/Completion.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+// import { withRouter } from 'react-router-dom'
+// import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+// import StatusMessages, { useMessages } from './StatusMessages'
+
+// import './App.css';
+
+const Completion = () => {
+
+
+
+
+
+
+
+
+    return (
+
+        <>
+
+        <a href="/">home</a>
+        <h1 style={{ textAlign:"center" }}>Confirmation</h1>
+
+        <form id="payment-form" >
+            <label style={{ textAlign:"center" }} htmlFor="name">
+                Payment was successful!!!
+            </label>
+
+            <div id="error-message" role="alert"></div>
+
+        </form>
+
+        {/* <div id="messages" role="alert"></div> */}
+
+
+
+
+    </>
+
+
+
+
+
+    )
+}
+
+export default Completion;
---
+++ client/src/Payment.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
+import { loadStripe } from "@stripe/stripe-js";
+
+function Payment() {
+  const [stripePromise, setStripePromise] = useState(null);
+  const [clientSecret, setClientSecret] = useState("");
+
+  useEffect(() => {
+    fetch("/config").then(async (r) => {
+      const { publishableKey } = await r.json();
+      setStripePromise(loadStripe(publishableKey));
+    });
+  }, []);
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(
+        { 
+          items: { id: "xl-tshirt" }, 
+          price: { id: "11000" },
+          customer: { id: "swt" }
+        }
+        ),
+      // body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
+
+  return (
+    <>
+      <h1>React Stripe and the Payment Element, Test:1</h1>
+      {clientSecret && stripePromise && (
+        <Elements stripe={stripePromise} options={{ clientSecret }}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </>
+  );
+}
+
+export default Payment;
